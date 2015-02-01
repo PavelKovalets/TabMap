bc94fc3826d20fe93fe5d26c3cfe13c892c3b740
Pavel Kovalets <kovalets.p.e@hotmail.com>
2015-02-01 21:12:46 +0300

    display geocoded objects on map

--- ChromeExtension/YandexMaps/yandex_maps.js
+++ ChromeExtension/YandexMaps/yandex_maps.js
@@ -1,9 +1,12 @@
 console.log("Hello from maps!");
 
+var parsedData = [];
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log("Message from the extension:");
         console.log(request.data);
+        parsedData = request.data;
     });
 
 var myMap;
@@ -17,8 +20,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
         myMap = new ymaps.Map('map', {
             // При инициализации карты обязательно нужно указать
             // её центр и коэффициент масштабирования.
-            center: [55.76, 37.64], // Москва
+            center: [53.929498, 27.564527], // Минск
             zoom: 10
         });
+
+        geocodeParsedData(myMap);
     }
+
+    function geocodeParsedData(map){
+        parsedData.forEach(function(d){
+           geocodeAddress(map, removeDistrictFromAddress(d.address));
+        });
+
+    };
+
+    function geocodeAddress(map, address) {
+        var myGeocoder = ymaps.geocode(address);
+        myGeocoder.then(
+            function (res) {
+                map.geoObjects.add(res.geoObjects);
+                // Выведем в консоль данные, полученные в результате геокодирования объекта.
+                console.log(res.geoObjects.get(0).properties.get('metaDataProperty').getAll());
+            },
+            function (err) {
+                // обработка ошибки
+                console.log(err);
+            }
+        );
+    };
+
+    function removeDistrictFromAddress (address) {
+        var districtStart = address.indexOf("(");
+        return address.substring(0, districtStart);
+    };
 });

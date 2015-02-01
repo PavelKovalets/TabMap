49c3c5aa80a798bf4146704b0e43ac5ed3351a47
Pavel Kovalets <kovalets.p.e@hotmail.com>
2015-02-01 19:21:32 +0300

    add support for Yandex.Maps

--- ChromeExtension/YandexMaps/yandex_maps.js
+++ ChromeExtension/YandexMaps/yandex_maps.js
@@ -5,3 +5,20 @@ chrome.runtime.onMessage.addListener(
         console.log("Message from the extension:");
         console.log(request.data);
     });
+
+var myMap;
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    ymaps.ready(init);
+
+    function init () {
+        // Создание экземпляра карты и его привязка к контейнеру с
+        // заданным id ("map").
+        myMap = new ymaps.Map('map', {
+            // При инициализации карты обязательно нужно указать
+            // её центр и коэффициент масштабирования.
+            center: [55.76, 37.64], // Москва
+            zoom: 10
+        });
+    }
+});
